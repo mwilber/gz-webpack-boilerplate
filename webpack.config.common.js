f6cbc08ab59e469cadcb217ed54483745718bf1a
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2019-05-10 08:11:52 -0400

    Add support for svg.

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -85,7 +85,7 @@ module.exports = {
 
             // IMAGES
             {
-                test: /\.(jpe?g|png|gif)$/,
+                test: /\.(jpe?g|png|gif|svg)$/,
                 loader: 'file-loader',
                 options: {
                     name: '[path][name].[ext]'
